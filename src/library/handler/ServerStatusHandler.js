5ddee57a114601ede24760009ad55205f69461cf
Nick Spijker <33859387+Nick026@users.noreply.github.com>
2021-04-10 18:53:24 +0200

    Playercount functionality (#2)
    Playercount functionality

---
+++ src/library/handler/ServerStatusHandler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Handler = require('./Handler');
+
+let race = {
+	player_count: 0,
+	max_players: 0
+};
+
+let mix = {
+	player_count: 0,
+	max_players: 0
+};
+
+class ServerStatusHandler extends Handler {
+	constructor() {
+		super();
+
+		this.types.add('server.player_count_race');
+		this.types.add('server.player_count_mix');
+	}
+
+	execute(bot, session, type, payload) {
+		if (type === 'server.player_count_race') {
+			race = payload;
+		} else if (type === 'server.player_count_mix') {
+			mix = payload;
+		}
+
+		bot.setActivity(`Race: ${race.player_count}/${race.max_players} | Mix: ${mix.player_count}/${mix.max_players}`);
+	}
+}
+
+module.exports = ServerStatusHandler;
